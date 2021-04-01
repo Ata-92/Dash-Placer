6b6ea84290960adca473572b1295d6698f347545
Ata-92 <atakal92@gmail.com>
2021-04-01 16:33:52 +0300

    Add dash between even numbers

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 function dashPlacer(num) {
-  if (isNaN(num)) {
+  if (isNaN(String(num))) {
     console.log(`Your input ${num} is not a number.
-                Please try again.`)
+                Please try again.`);
+  } else {
+    let newNum = "";
+    for (let i = 0; i < String(num).length; i++) {
+      if (parseInt(String(num)[i]) % 2 === 0 && parseInt(String(num)[i + 1]) % 2 === 0) {
+        newNum += String(num)[i] + "-";
+      } else {
+        newNum += String(num)[i];
+      }
+    }
+    console.log(newNum);
   }
 }
+
+dashPlacer(0204568780);
