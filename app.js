f954d8abc1dea6d85ad30261d167af75cef6b7a8
Ata-92 <atakal92@gmail.com>
2021-04-01 16:39:42 +0300

    Remove unnecessary if number checked

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 function dashPlacer(num) {
-  if (isNaN(String(num))) {
-    console.log(`Your input ${num} is not a number.
-                Please try again.`);
-  } else {
-    let newNum = "";
-    for (let i = 0; i < String(num).length; i++) {
-      if (parseInt(String(num)[i]) % 2 === 0 && parseInt(String(num)[i + 1]) % 2 === 0) {
-        newNum += String(num)[i] + "-";
-      } else {
-        newNum += String(num)[i];
-      }
+  let newNum = "";
+  for (let i = 0; i < String(num).length; i++) {
+    if (parseInt(String(num)[i]) % 2 === 0 && parseInt(String(num)[i + 1]) % 2 === 0) {
+      newNum += String(num)[i] + "-";
+    } else {
+      newNum += String(num)[i];
     }
-    console.log(newNum);
   }
+  console.log(newNum);
 }
 
-dashPlacer(0204568780);
+dashPlacer(102045687801);
